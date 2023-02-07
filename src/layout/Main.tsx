868909c5ce8189b821c6b6fb491fe60f0957556b
dongqui <dongqui9@gmail.com>
2023-02-07 22:25:00 +0900

    feat: mobile 내용 padding 조절

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,12 +1,18 @@
 import { css } from '@emotion/react';
 import { PropsWithChildren } from 'react';
 
+import { useMediaQuery } from 'hooks';
+import { MOBILE_WIDTH } from 'constant';
+
 export default function Main({ children }: PropsWithChildren) {
+  const isMobile = useMediaQuery(MOBILE_WIDTH);
+
+  const padding = isMobile ? 24 : 96;
   return (
     <main
       css={css`
-        padding-left: calc(96px + env(safe-area-inset-left));
-        padding-right: calc(96px + env(safe-area-inset-right));
+        padding-left: calc(${padding}px + env(safe-area-inset-left));
+        padding-right: calc(${padding}px + env(safe-area-inset-right));
         padding-top: 80px;
       `}
     >
