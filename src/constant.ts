838d25f0c251f6b31f28356930a483c0f8b4fad7
dongqui <dongqui9@gmail.com>
2023-02-07 23:48:23 +0900

    fix: 사이드바 기본 넓이 변경

--- src/constant.ts
+++ src/constant.ts
@@ -2,4 +2,4 @@ export const TABLIT_WIDTH = 820;
 export const MOBILE_WIDTH = 820;
 export const SIDEBAR_MINIMUM_WIDTH = 220;
 export const SIDEBAR_MAXIMUM_WIDTH = 480;
-export const SIDEBAR_DEFAULT_WIDTH = 230;
+export const SIDEBAR_DEFAULT_WIDTH = 280;
