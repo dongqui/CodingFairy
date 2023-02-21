dcaa3c95d3399a58b7325e35ba06e7c9399c15bc
dongqui <dongqui9@gmail.com>
2023-02-21 18:38:06 +0900

    feat: 공통 head 정의

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="author" content="CodingFairy" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://www.codingfairy.org/"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
