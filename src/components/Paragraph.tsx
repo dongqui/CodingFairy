c51bd2b152be283bc3160dfe929bac002cdd2046
dongqui <dongqui9@gmail.com>
2023-02-26 22:27:07 +0900

    fix: 텍스트 내부에 있는 HTML tag 렌더링할 수 있도록 수정

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,30 +1,27 @@
 import { css } from '@emotion/react';
-import { PropsWithChildren, Fragment } from 'react';
+import { PropsWithChildren } from 'react';
 
-interface Props {
-  stringToJSX?: boolean;
-}
+interface Props {}
 
-export default function Paragraph({ children, stringToJSX = true }: PropsWithChildren<Props>) {
-  return (
+export default function Paragraph({ children }: PropsWithChildren<Props>) {
+  return typeof children === 'string' ? (
     <p
       css={css`
         font-size: 1.6rem;
         line-height: 1.5;
         color: rgba(255, 255, 255, 0.81);
         white-space: pre-wrap;
+        margin: 0;
+        strong {
+          color: rgba(255, 255, 255, 0.81);
+          font-size: 1.8rem;
+          line-height: 1.5;
+          font-weight: bolder;
+        }
       `}
-    >
-      {stringToJSX === true && typeof children === 'string' ? convertNewLineToJSX(children) : children}
-    </p>
+      dangerouslySetInnerHTML={{ __html: children }}
+    />
+  ) : (
+    <p>{children}</p>
   );
-}
-
-function convertNewLineToJSX(str: string) {
-  return str.split('\n').map((line, index) => (
-    <Fragment key={index}>
-      {index > 0 ? <br /> : ''}
-      {line}
-    </Fragment>
-  ));
 }
